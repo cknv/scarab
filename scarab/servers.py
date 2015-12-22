16b2597f43e8fc4779f849a4798780af6c146fc8
Esben Sonne <esbensonne@gmail.com>
2015-12-22 22:58:30 +0100

    Added a simple server for previewing
    Unlike other attempts, this one does not rely on files on the
    filesystem, but instead keeps pages in memory. Which is fine for
    smaller sites.

---
+++ scarab/servers.py
@@ -0,0 +1,48 @@
+"""Scarab servers."""
+from http import server
+
+
+class PreviewServer:
+
+    """A server for previewing pages locally."""
+
+    def __init__(self, port):
+        """Create a new PreviewServer."""
+        server_address = ('', port)
+        self._server = server.HTTPServer(server_address, PreviewHandler)
+        self._server.pages = {}
+
+    def set_pages(self, pages):
+        """Set the servers pages."""
+        self._server.pages = {
+            '/' + page['destination']: page['bytes']
+            for page in pages
+        }
+
+    def serve_forever(self):
+        """Serve forever, just like the inner server."""
+        self._server.serve_forever()
+
+
+class PreviewHandler(server.BaseHTTPRequestHandler):
+
+    """Handler for the PreviewServer."""
+
+    def load_page(self, path):
+        """Load a page, or raise KeyError."""
+        if path.endswith('/'):
+            path = path + 'index.html'
+
+        return self.server.pages[path]
+
+    def do_GET(self):
+        """Handle GET requests."""
+        try:
+            content = self.load_page(self.path)
+        except KeyError:
+            self.send_response(404)
+            self.end_headers()
+        else:
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(content)
