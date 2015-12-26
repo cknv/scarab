fc4a112c45f56a9a076ec342aef96a76af0764d4
Esben Sonne <esbensonne@gmail.com>
2015-12-26 09:38:02 +0100

    Refactored dir prepending out

--- scarab/writers.py
+++ scarab/writers.py
@@ -5,14 +5,11 @@ import os
 def bytes_writer(pages, output_directory):
     """Write items to directory."""
     for page in pages:
-        prefixed_path = os.path.join(
-            output_directory,
-            page.path,
-        )
+        destination_path = prepend_dir(output_directory, page.path)
 
-        ensure_directory(prefixed_path)
-        print(prefixed_path)
-        with open(prefixed_path, 'bw+') as fo:
+        ensure_directory(destination_path)
+        print(destination_path)
+        with open(destination_path, 'bw+') as fo:
             fo.write(page.bytes)
 
 
@@ -21,3 +18,11 @@ def ensure_directory(path):
     destination_folder = os.path.dirname(path)
     if not os.path.exists(destination_folder):
         os.makedirs(destination_folder)
+
+
+def prepend_dir(directory, path):
+    """Prepend a directory onto a path."""
+    return os.path.join(
+        directory,
+        path,
+    )
